3230ca0ca376815be672f0ef948bc9ad05f8c641
Aleks Bekker <alekso.bekker@gmail.com>
2025-09-08 16:56:10 -0400

    Add PostHog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
+import type { ConfigDefaults } from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { AppRouter } from "./AppRouter.tsx";
+import "./index.css";
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: "2025-05-24" as ConfigDefaults,
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppRouter />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <AppRouter />
+    </PostHogProvider>
   </StrictMode>,
 );
